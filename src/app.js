866b1d9ec102f52d0ac55239c603f804b082648a
Doc-Jebus <saunders_wyndham@hotmail.com>
2022-08-31 17:06:54 +0900

    Styling completed.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,28 @@
-const jobPostBtn = document.querySelector('#job-post-btn');
-const modalCloseBtn = document.querySelector('#modal-close-btn');
-const overlay = document.querySelector('#overlay');
+const jobPostBtn = document.getElementById('job-post-btn');
+const closeModalBtn = document.getElementById('modal-close-btn');
+const modalForm = document.getElementById('modal-form');
+const overlay = document.getElementById('overlay');
+const inputs = document.querySelectorAll('input');
 
-jobPostBtn.addEventListener('click', (e) => {
+
+
+jobPostBtn.addEventListener('click', () => {
   
+  modalForm.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 }) 
+
+overlay.addEventListener('click', () => {
+  inputs.forEach(input => input.value = '');
+  modalForm.classList.add('hidden');
+  overlay.classList.remove('hidden');
+  })
+
+closeModalBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  inputs.forEach(input => input.value = '');
+  modalForm.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
+  
+});
 
