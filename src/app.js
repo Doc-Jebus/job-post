e3909cac2fe51368d9e0a705223f283bec419ecc
Doc-Jebus <saunders_wyndham@hotmail.com>
2022-09-01 18:07:35 +0900

    Store array in local storage.

--- src/app.js
+++ src/app.js
@@ -7,10 +7,10 @@ const overlay = document.getElementById('overlay');
 const inputs = document.querySelectorAll('input');
 
 //Form input values
+const jobPostForm = document.getElementById('job-post-form');
 const companyName = document.getElementById('company-name').value;
-console.log(companyName);
 const jobTitle = document.getElementById('job-title').value;
-const salary = document.getElementById('job-salary').value;
+const jobSalary = document.getElementById('job-salary').value;
 const jobLocation = document.getElementById('job-location').value;
 const onlineStatus = document.querySelector('input[name="online-onsite"]:checked').value;
 const applicationStartDate = document.getElementById('application-period-start').value;
@@ -20,11 +20,14 @@ const jobDescription = document.getElementById('job-description').value;
 
 let jobListings = [];
 
+let jobsPosted = JSON.parse(localStorage.getItem("job_list"));
+
 class JobPost {
   constructor(company, jobPosition, location, salary, applicationStartDate, applicationEndDate, jobDescription) {
     this.company = company;
     this.jobPosition = jobPosition;
     this.location = location;
+    //this.jobStatus = jobStatus;
     this.salary = salary;
     this.applicationStartDate = applicationStartDate;
     this.applicationEndDate = applicationEndDate;
@@ -32,11 +35,34 @@ class JobPost {
   }
 }
 
-function addJobToList() {
-  
-}
 
-//Button to open new jon post.
+jobSubmitBtn.addEventListener('click', (e)=> {
+  e.preventDefault();
+  let job = new JobPost(
+    document.getElementById('company-name').value, 
+    document.getElementById('job-title').value,
+    document.getElementById('job-location').value, 
+    //document.querySelectorAll('input[name="online-onsite"]:checked').value,
+    document.getElementById('job-salary').value, 
+    document.getElementById('application-period-start').value, 
+    document.getElementById('application-period-end').value,
+    document.getElementById('job-description').value)
+  console.log(job);
+
+  let currentJobEntries = JSON.parse(localStorage.getItem("jobListings"));
+
+  if(currentJobEntries == null) {
+    jobListings = [];
+  } else {
+    alert("Job already posted");
+  }
+  currentJobEntries.push(job);
+  localStorage.setItem("job_list", JSON.stringify(jobListings));
+});
+
+//function addJobToList() {}
+
+//Button to open new job post.
 jobPostBtn.addEventListener('click', () => {
   modalForm.classList.remove('hidden');
   overlay.classList.remove('hidden');
